b49777518ec92c1a96ebd76e4f6f42852c0eea63
ScarX80 <oscargal@gmail.com>
2024-05-19 14:56:27 +0000

    Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer style={styles.footer}>
-      <p style={styles.text}>© 2024 BinariaOS.</p>
+      <p style={styles.text}>© 2024 BinariaOS</p>
     </footer>
   );
 };
