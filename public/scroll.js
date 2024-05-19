f3e45a96e64247d8f676d5ca6d5dcf20a6f3d6a2
ScarX80 <oscargal@gmail.com>
2024-05-19 14:14:54 +0000

    Hidden Scroll

--- public/scroll.js
+++ public/scroll.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const rootElement = document.getElementById('root');
+    const scrollContainer = document.querySelector('.scroll-container');
   
     // Desplazarse hacia abajo al hacer clic o tap en cualquier lugar del contenedor
-    rootElement.addEventListener('click', () => {
-      rootElement.scrollBy({
+    scrollContainer.addEventListener('click', () => {
+      scrollContainer.scrollBy({
         top: window.innerHeight,
         behavior: 'smooth'
       });
     });
   
     // Desplazarse hacia arriba si ya estás en la parte inferior del contenedor
-    rootElement.addEventListener('touchend', () => {
-      if (rootElement.scrollTop + rootElement.clientHeight >= rootElement.scrollHeight) {
-        rootElement.scrollTo({
+    scrollContainer.addEventListener('touchend', () => {
+      if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
+        scrollContainer.scrollTo({
           top: 0,
           behavior: 'smooth'
         });
